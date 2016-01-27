9944cddad2e3145213c6ea7e15fe00df0e5c98ba
Ronald91 <rwilliams0291@yahoo.com>
2016-01-26 20:33:59 -0500

    1/4 way through of Step 2, I see I will start to deviate around here

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,12 @@ interface Ranger {
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
+        <h2>My Rangers</h2>
+        <ul class="rangers">
+            <li *ngFor="#ranger of rangers">\n\
+                <span class="badge">{{ranger.id}}</span> {{ranger.name}}
+            </li>
+        </ul>
         <h2>{{ranger.name}} details!</h2>
         <div><label>id: </label>{{ranger.id}}</div>
         <div>
@@ -25,4 +31,20 @@ export class AppComponent {
         id: 6,
         name: 'Charizard'
     };
+
+    public rangers = RANGERS;
+
 }
+
+var RANGERS: Ranger[] = [
+    { id: 6, name: 'Charizard' },
+    { id: 160, name: 'Feraligatr' },
+    { id: 149, name: 'Dragonite' },
+    { id: 373, name: 'Salamence' },
+    { id: 65, name: 'Alakazam' },
+    { id: 150, name: 'Mewtwo ' },
+    { id: 635, name: 'Hydreigon' },
+    { id: 244, name: 'Entei' },
+    { id: 249, name: 'Lugia' },
+    { id: 151, name: 'Mew' }
+];
